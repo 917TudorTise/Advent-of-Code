ff1f9416cb0f31914d9f19981a62c8def796fe78
917TudorTise <tudor_tise@yahoo.com>
2020-12-01 16:53:18 +0200

    Day 1, problem 2 done

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,5 +1,6 @@
 f = open("scratch.txt", "r")
-g = open("solution.txt", "a")
+sol1 = open("solution.txt", "a")
+sol2 = open("solution2.txt", "a")
 
 numbers = []
 
@@ -9,7 +10,14 @@ for x in f:
 for i in range(len(numbers) - 1):
     for j in range(i + 1, len(numbers)):
         if numbers[i] + numbers[j] == 2020:
-            g.write(str(numbers[i] * numbers[j]))
+            sol1.write(str(numbers[i] * numbers[j]))
+
+for i in range(len(numbers) - 2):
+    for j in range(i + 1, len(numbers) - 1):
+        for k in range(j + 1, len(numbers)):
+            if numbers[i] + numbers[j] + numbers[k] == 2020:
+                sol2.write(str(numbers[i] * numbers[j] * numbers[k]))
 
 f.close()
-g.close()
+sol1.close()
+sol2.close()
