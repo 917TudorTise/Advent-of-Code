f3331641d046fc6f8ba47b4b77ec6d3efd2bc560
917TudorTise <tudor_tise@yahoo.com>
2020-12-02 09:17:46 +0200

    Day 2, problem 2 done

--- Day 2/main.py
+++ Day 2/main.py
@@ -3,6 +3,9 @@ f = open("data1.txt", "r")
 
 nr_valid_pass = 0
 
+nr_valid_second_problem = 0
+
+
 while True:
 
     is_ok = True
@@ -19,14 +22,16 @@ while True:
     number_of_occurrences = 0
     for each in password:
         if each == char:
-            number_of_occurrences +=1
+            number_of_occurrences += 1
 
     if number_of_occurrences < limits[0] or number_of_occurrences > limits[1]:
         is_ok = False
 
+    if (password[limits[0]-1] == char and password[limits[1]-1] != char) or (password[limits[0]-1] != char and password[limits[1]-1] == char):
+        nr_valid_second_problem += 1
+
     if is_ok:
-        nr_valid_pass +=1
-
-
+        nr_valid_pass += 1
 
 print("There are ", nr_valid_pass, " number of valid passwords")
+print("There are ", nr_valid_second_problem, " number of valid passwords for the second problem")
